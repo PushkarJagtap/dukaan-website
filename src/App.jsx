462bf76e5d46a51e6d08c3404894a5ce29a93446
PushkarJagtap <pushkarjagtap32@outlook.com>
2025-07-29 23:06:22 +0530

    created-header-hero-marquee

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/header/Header'
+import Hero from './hearo-section/Hero'
+import Marquee from './marquee/Marquee'
+function App() {
+ 
+
+  return (
+    <>
+    <Header></Header>
+    <Hero></Hero>
+    <Marquee></Marquee>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import './header.css'
+function Header(){
+    return(
+        <header>
+                <div className="header-left">
+                <img src="public/logo.svg"></img>
+                 <h1>
+                 <ul><a href='/'>Products</a></ul>
+                    <ul><a href='/'>Company</a></ul>
+                   <ul><a href='/'>Resources</a></ul>
+                   <ul><a href='/'>Pricing</a></ul></h1>                   
+            </div>
+            <div className="header-right">
+                <h1>
+                
+                    <a href='/'>Sign in</a>
+                    <a href='/'>Start Free</a>
+                </h1>
+            </div>
+        </header>
+    )
+}
+export default Header;
