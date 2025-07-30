be933eed9877ee1f73fc8872d3ee64cca2d30ca9
PushkarJagtap <pushkarjagtap32@outlook.com>
2025-07-30 21:13:33 +0530

    second-content-img-text completed

--- src/second-main/Head2.jsx
+++ src/second-main/Head2.jsx
@@ -17,29 +17,29 @@ function Head2() {
        </div>
          </div>
         </div>
-         <div className="card1">
-         <img src="https://mydukaan.io/_next/static/images/launch-fast-6349265d2de0cf16e61e160f85bd681d.svg" alt="" width={550}/>
-         <div className="card-content">
-         <h1>Launch Fast</h1>
-         <div className='more-content'>
-         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Fully responsive e-commerce website & mobile app.</p>
-         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Loads 6X faster than existing solutions.</p>
-         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Upload/import products and inventory in bulk.</p>
-         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Integrate payment gateways</p>
-         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Easily customizable themes.</p>
+         <div className="card2">
+         <img src="https://mydukaan.io/_next/static/images/scale-faster-615089c61891323e0c2e2f0a4d498d53.svg" alt="" width={550}/>
+         <div className="card-content2">
+         <h1>Scale Faster</h1>
+         <div className='more-content2'>
+         <p><img src='https://mydukaan.io/_next/static/media/star-blue.f616f6970481413e436c6ec2ad621545.svg'></img> Guaranteed 99.5% uptime for your store - We keep you open for business.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-blue.f616f6970481413e436c6ec2ad621545.svg'></img> 60+ third party plugins.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-blue.f616f6970481413e436c6ec2ad621545.svg'></img> Marketing tools and discounts to drive repeat orders</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-blue.f616f6970481413e436c6ec2ad621545.svg'></img> Add staff accounts, assign different roles.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-blue.f616f6970481413e436c6ec2ad621545.svg'></img> Unlimited transactions, minimal transaction fees.</p>
        </div>
          </div>
         </div>
-               <div className="card1">
-         <img src="https://mydukaan.io/_next/static/images/launch-fast-6349265d2de0cf16e61e160f85bd681d.svg" alt="" width={550}/>
-         <div className="card-content">
-         <h1>Launch Fast</h1>
-         <div className='more-content'>
-         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Fully responsive e-commerce website & mobile app.</p>
-         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Loads 6X faster than existing solutions.</p>
-         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Upload/import products and inventory in bulk.</p>
-         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Integrate payment gateways</p>
-         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Easily customizable themes.</p>
+               <div className="card3">
+         <img src="https://mydukaan.io/_next/static/images/manage-better-5f03835fb0602b9fa6a4b3fc98bc9c7c.svg" alt="" width={550}/>
+         <div className="card-content3">
+         <h1>Manage Better</h1>
+         <div className='more-content3'>
+         <p><img src='https://mydukaan.io/_next/static/media/star-gray.1f0c906d374a9e78df3310d226fb00d9.svg'></img> Order tracking, invoicing and order reports.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-gray.1f0c906d374a9e78df3310d226fb00d9.svg'></img> Bulk edit product prices, variants, inventory.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-gray.1f0c906d374a9e78df3310d226fb00d9.svg'></img> Manage global deliveries.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-gray.1f0c906d374a9e78df3310d226fb00d9.svg'></img> In-depth business analytics.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-gray.1f0c906d374a9e78df3310d226fb00d9.svg'></img> Automate all tax calculations.</p>
        </div>
          </div>
         </div>
