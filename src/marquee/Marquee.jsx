7e221c164d873fe9ee5ec5ba7d78ba4a923188b1
PushkarJagtap <pushkarjagtap32@outlook.com>
2025-07-30 14:32:07 +0530

    marquee - updated

--- src/marquee/Marquee.jsx
+++ src/marquee/Marquee.jsx
@@ -58,6 +58,30 @@ function Marquee() {
             alt="CodePen Logo"
           />
         </div>
+           <div class="scroll-card">
+          <img
+            src="https://mydukaan.io/_next/static/images/uppercase-f225d6aa2bf2a3ff7bee2e2f7c352fb2.svg"
+            alt="CodePen Logo"
+          />
+        </div>
+           <div class="scroll-card">
+          <img
+            src="https://mydukaan.io/_next/static/images/uppercase-f225d6aa2bf2a3ff7bee2e2f7c352fb2.svg"
+            alt="CodePen Logo"
+          />
+        </div>
+           <div class="scroll-card">
+          <img
+            src="https://mydukaan.io/_next/static/images/uppercase-f225d6aa2bf2a3ff7bee2e2f7c352fb2.svg"
+            alt="CodePen Logo"
+          />
+        </div>
+           <div class="scroll-card">
+          <img
+            src="https://mydukaan.io/_next/static/images/uppercase-f225d6aa2bf2a3ff7bee2e2f7c352fb2.svg"
+            alt="CodePen Logo"
+          />
+        </div>
       </div>
     </div>
   );
