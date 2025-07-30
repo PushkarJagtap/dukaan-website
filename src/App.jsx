e595db88091f66e887d620b2603ea95589724db2
PushkarJagtap <pushkarjagtap32@outlook.com>
2025-07-30 20:00:19 +0530

    second-content-img-text added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Header from './components/header/Header'
 import Hero from './hearo-section/Hero'
 import Marquee from './marquee/Marquee'
+import Head2 from './second-main/Head2'
 function App() {
   return (
     <>
     <Header></Header>
     <Hero></Hero>
     <Marquee></Marquee>
+    <Head2></Head2>
     </>
   )
 }
---
+++ src/second-main/Head2.jsx
@@ -0,0 +1,51 @@
+import './head2.css'
+function Head2() {
+  return (
+    <>
+    <div className='heading2'>
+        <h1>Whether you’re a startup or an established business, here’s why Dukaan is your best choice</h1></div>
+        <div className="card1">
+         <img src="https://mydukaan.io/_next/static/images/launch-fast-6349265d2de0cf16e61e160f85bd681d.svg" alt="" width={550}/>
+         <div className="card-content">
+         <h1>Launch Fast</h1>
+         <div className='more-content'>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Fully responsive e-commerce website & mobile app.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Loads 6X faster than existing solutions.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Upload/import products and inventory in bulk.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Integrate payment gateways</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Easily customizable themes.</p>
+       </div>
+         </div>
+        </div>
+         <div className="card1">
+         <img src="https://mydukaan.io/_next/static/images/launch-fast-6349265d2de0cf16e61e160f85bd681d.svg" alt="" width={550}/>
+         <div className="card-content">
+         <h1>Launch Fast</h1>
+         <div className='more-content'>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Fully responsive e-commerce website & mobile app.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Loads 6X faster than existing solutions.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Upload/import products and inventory in bulk.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Integrate payment gateways</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Easily customizable themes.</p>
+       </div>
+         </div>
+        </div>
+               <div className="card1">
+         <img src="https://mydukaan.io/_next/static/images/launch-fast-6349265d2de0cf16e61e160f85bd681d.svg" alt="" width={550}/>
+         <div className="card-content">
+         <h1>Launch Fast</h1>
+         <div className='more-content'>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Fully responsive e-commerce website & mobile app.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Loads 6X faster than existing solutions.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Upload/import products and inventory in bulk.</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Integrate payment gateways</p>
+         <p><img src='https://mydukaan.io/_next/static/media/star-purple.3a0f99650c0ab639cf633cf46e9a29ae.svg'></img> Easily customizable themes.</p>
+       </div>
+         </div>
+        </div>
+        
+    </>
+  )
+}
+
+export default Head2
